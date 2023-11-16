c6357eeabb23e12963929f6846c9b7fae2a22b9f
kaixih <kaixih@nvidia.com>
2023-11-16 11:26:02 -0800

    Clean up the tests

--- L2_xla_fp8_benchmark/base.sh
+++ L2_xla_fp8_benchmark/base.sh
@@ -3,23 +3,10 @@ PAXML_DIR=$(dirname `python -c 'import paxml; print(*paxml.__path__)'`)
 cp ci_configs.py ${PAXML_DIR}
 pushd ${PAXML_DIR} > /dev/null
 
-# Use fake datasets
-export VOCAB_PATH="/home/dataset/c4_en_301_5Mexp2_spm.model"
-
-FP8_COMMON="--xla_gpu_enable_reduction_epilogue_fusion=false \
-            --xla_gpu_enable_cublaslt=true \
-           "
-XLA_COMMON="--xla_gpu_enable_latency_hiding_scheduler=true \
-            --xla_gpu_enable_async_collectives=true \
-            --xla_gpu_enable_highest_priority_async_stream=true \
-            --xla_gpu_all_reduce_combine_threshold_bytes=51200 \
-            --xla_gpu_enable_cudnn_layer_norm=true \
-            --xla_gpu_enable_cudnn_fmha=false \
-           "
 MODEL_NAME=$1
 OPT_MODE=$2
 MATH_MODE=$3
-TRITON_GEMM=$4
+XLA_EXTRAS=$4
 G=$5
 
 if [[ "$OPT_MODE" == "XLA" ]]; then
@@ -31,7 +18,6 @@ else
   exit 1
 fi
 if [[ "$MATH_MODE" == "fp8" ]]; then
-  XLA_COMMON+=$FP8_COMMON
   USE_FP8=true
   export ENABLE_FP8=1
 elif [[ "$MATH_MODE" == "bf16" ]]; then
@@ -41,16 +27,40 @@ else
   echo TRAINING SCRIPT FAILED: Unsupported MATH_MODE: $MATH_MODE
   exit 1
 fi
-if [[ "$TRITON_GEMM" == "no" ]]; then
-  USE_TRITON_GEMM=false
-elif [[ "$TRITON_GEMM" == "yes" ]]; then
-  USE_TRITON_GEMM=true
-else
-  echo TRAINING SCRIPT FAILED: Unsupported TRITON_GEMM: $TRITON_GEMM
-  exit 1
+USE_CUBLASLT="false"
+USE_CUDNN_LN="false"
+USE_CUDNN_FMHA="false"
+USE_TRITON_GEMM="false"
+for F in $(echo $XLA_EXTRAS | sed 's/,/ /g'); do
+  case "$F" in
+    cublaslt) USE_CUBLASLT="true" ;;
+    cudnn_ln) USE_CUDNN_LN="true" ;;
+    cudnn_fmha) USE_CUDNN_FMHA="true" ;;
+    triton_gemm) USE_TRITON_GEMM="true" ;;
+    none) ;;
+    *) echo Invalid XLA_EXTRAS $F; exit 1 ;;
+  esac
+done
+
+# Use fake datasets
+export VOCAB_PATH="/home/dataset/c4_en_301_5Mexp2_spm.model"
+
+FP8_COMMON="--xla_gpu_enable_reduction_epilogue_fusion=false \
+           "
+XLA_COMMON="--xla_gpu_enable_latency_hiding_scheduler=true \
+            --xla_gpu_enable_async_collectives=true \
+            --xla_gpu_enable_highest_priority_async_stream=true \
+            --xla_gpu_all_reduce_combine_threshold_bytes=51200 \
+            --xla_gpu_enable_cudnn_layer_norm=$USE_CUDNN_LN \
+            --xla_gpu_enable_cudnn_fmha=$USE_CUDNN_FMHA \
+            --xla_gpu_enable_cublaslt=$USE_CUBLASLT \
+            --xla_gpu_enable_triton_gemm=$USE_TRITON_GEMM \
+           "
+if [[ "$MATH_MODE" == "fp8" ]]; then
+  XLA_COMMON+=$FP8_COMMON
 fi
 
-export XLA_FLAGS="$XLA_COMMON --xla_gpu_enable_triton_gemm=$USE_TRITON_GEMM"
+export XLA_FLAGS="$XLA_COMMON"
 SECONDS=0
 TMPFILE="$TMPDIR/$(mktemp tmp.XXXXXX)"
 python -m paxml.main \
@@ -86,52 +96,7 @@ if [[ $FAILURE -ne 0 ]]; then
   exit 1
 fi
 
-printf "%-30s %7s %4s %11s %5s %9s %8s\n" $MODEL_NAME $OPT_MODE $MATH_MODE $TRITON_GEMM $G $PERF $WALLTIME
+printf "%-18s %8s %4s %35s %4d %9.3f %8d\n" $MODEL_NAME $OPT_MODE $MATH_MODE $XLA_EXTRAS $G $PERF $WALLTIME
 rm -rf "$TMP_DIR"
 
-#echo -n 'ref(triton) '
-#export XLA_FLAGS="$XLA_COMM"
-#python -m paxml.main \
-#    --fdl_config=ci_configs.GPT5BSynthetic \
-#    --fdl.PACKED_INPUT=False \
-#    --fdl.USE_FP8=False \
-#    --job_log_dir=${OUTPUT} \
-#    --enable_checkpoint_saving=False \
-#    --alsologtostderr |& grep 'Setting task status: step = 100,.*steps/sec'
-#
-#echo -n 'ref(no_triton) '
-#export XLA_FLAGS="$XLA_COMM --xla_gpu_enable_triton_gemm=false"
-#python -m paxml.main \
-#    --fdl_config=ci_configs.GPT5BSynthetic \
-#    --fdl.PACKED_INPUT=False \
-#    --fdl.USE_FP8=False \
-#    --job_log_dir=${OUTPUT} \
-#    --enable_checkpoint_saving=False \
-#    --alsologtostderr |& grep 'Setting task status: step = 100,.*steps/sec'
-#
-#
-#echo -n 'fp8(triton) '
-#export XLA_FLAGS="$XLA_COMM $FP8_COMM"
-#python -m paxml.main \
-#    --fdl_config=ci_configs.GPT5BSynthetic \
-#    --fdl.PACKED_INPUT=False \
-#    --fdl.USE_FP8=True \
-#    --job_log_dir=${OUTPUT} \
-#    --enable_checkpoint_saving=False \
-#    --alsologtostderr |& grep 'Setting task status: step = 100,.*steps/sec'
-
-
-#export ENABLE_TE=1
-#export ENABLE_FP8=1
-#echo -n 'te-fp8 '
-#export XLA_FLAGS="$XLA_COMM --xla_gpu_enable_triton_gemm=false"
-#python -m paxml.main \
-#    --fdl_config=ci_configs.GPT5BSynthetic \
-#    --fdl.PACKED_INPUT=False \
-#    --job_log_dir=${OUTPUT} \
-#    --enable_checkpoint_saving=False \
-#    --alsologtostderr
-
-
- 
 
--- L2_xla_fp8_benchmark/test.sh
+++ L2_xla_fp8_benchmark/test.sh
@@ -1,11 +1,15 @@
 
-printf "%-30s %7s %4s %11s %5s %9s %8s\n" NETWORK OPT_MODE MATH TRITON_GEMM GPUs STEPS/SEC WALLSECS
+printf "%-18s %8s %4s %35s %4s %9s %8s\n" NETWORK OPT_MODE MATH XLA_EXTRAS GPUs STEPS/SEC WALLSECS
 
-bash base.sh GPT5BSynthetic XLA fp8 no 8
-bash base.sh GPT5BSynthetic XLA fp8 yes 8
-bash base.sh GPT5BSynthetic XLA bf16 no 8
-bash base.sh GPT5BSynthetic XLA bf16 yes 8
-
-#bash base.sh GPT175BSynthetic XLA fp8 no 8
-#bash base.sh GPT175BSynthetic XLA bf16 yes 8
+bash base.sh GPT5BSynthetic XLA fp8 cublaslt,cudnn_ln 8
+bash base.sh GPT5BSynthetic XLA bf16 triton_gemm,cudnn_ln,cudnn_fmha 8
+if [ "$1" == "full" ]; then
+  bash base.sh GPT5BSynthetic XLA fp8 none 8
+  bash base.sh GPT5BSynthetic XLA fp8 cublaslt 8
+  bash base.sh GPT5BSynthetic XLA fp8 triton_gemm 8
+  bash base.sh GPT5BSynthetic XLA fp8 cublaslt,cudnn_ln,cudnn_fmha 8
+  bash base.sh GPT5BSynthetic XLA bf16 none 8
+  bash base.sh GPT5BSynthetic XLA bf16 triton_gemm 8
+  bash base.sh GPT5BSynthetic XLA bf16 triton_gemm,cudnn_ln 8
+fi
 
