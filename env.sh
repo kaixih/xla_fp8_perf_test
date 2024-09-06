19d6c5ba4cfba6a9df30e8a02e7d716589d13cd4
kaixih <kaixih@nvidia.com>
2024-09-06 21:55:46 +0000

    Allow user provided env var to override

--- env.sh
+++ env.sh
@@ -1,8 +1,9 @@
 # TODO(kaixih): nvbug/4833198 and nvbug/4830674
-export XLA_COMMON=" \
+DEFAULT=" \
     --xla_gpu_enable_triton_gemm=false \
     --xla_gpu_enable_pipelined_all_reduce=false \
     --xla_gpu_enable_pipelined_all_gather=false \
     --xla_gpu_enable_pipelined_reduce_scatter=false \
 "
+export XLA_COMMON="${XLA_COMMON:-$DEFAULT}"
  
